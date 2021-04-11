85d234850706a3ceba471f4ca1bab2a00f9be19a
UoA-sjer215 <68827415+UoA-sjer215@users.noreply.github.com>
2021-04-11 21:45:25 +1200

    structure added
    just added things that might be used in the future

--- scripts/gui.py
+++ scripts/gui.py
@@ -1,3 +1,47 @@
-from PyQt5.QtWidgets import QMainWindow
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtGui import QIcon
+
+class App (QWidget):
+    def __init__ (self,num):
+        super().__init__()
+
+        if num==1 :
+            self.main(name)
+        elif num==2:
+            self.main(name)
+        elif num==3:
+            self.main(name)
 
 
+    
+    def main (self):
+        self.setWindowTitle("main")
+        self.move(300,300)
+        self.setWindowIcon(QIcon('cat.jpg'))
+        self.resize(300,600)
+        self.show()
+
+    def train (self):
+        self.setWindowTitle("training")
+        self.move(300,300)
+        self.setWindowIcon(QIcon('cat.jpg'))
+        self.resize(300,600)
+        self.show()
+    
+    def insert (self):
+        self.setWindowTitle("inserting digits")
+        self.move(300,300)
+        self.setWindowIcon(QIcon('cat.jpg'))
+        self.resize(300,600)
+        self.show()
+
+
+#creates the qapplication
+parent = QApplication(sys.argv)
+
+firstwindow = App(1)
+
+
+#execute the qapplication
+sys.exit(parent.exec())
