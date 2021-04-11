ef74d156cac9c842f417a03c0115e79fa357013f
UoA-sjer215 <68827415+UoA-sjer215@users.noreply.github.com>
2021-04-11 22:17:00 +1200

    added changing windows
    you can now change between windows

--- scripts/gui.py
+++ scripts/gui.py
@@ -1,18 +1,32 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QToolTip
 from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import QCoreApplication
+
 
 class App (QWidget):
     def __init__ (self,num):
         super().__init__()
 
         if num==1 :
-            self.main(name)
+            self.main()
         elif num==2:
-            self.main(name)
+            self.train()
         elif num==3:
-            self.main(name)
+            self.insert()
 
+
+    def ShowMain (self):
+        self.hide()
+        mainW.show()
+
+    def ShowTrain (self):
+        self.hide()
+        trainW.show()
+
+    def ShowInsert (self):
+        self.hide()
+        insertW.show()
 
     
     def main (self):
@@ -20,27 +34,61 @@ class App (QWidget):
         self.move(300,300)
         self.setWindowIcon(QIcon('cat.jpg'))
         self.resize(300,600)
+
+        train = QPushButton('train AI', self)
+        train.move(50, 50)
+        train.resize(train.sizeHint())
+        train.clicked.connect(self.ShowTrain)
+
+        insert = QPushButton('Insert digit', self)
+        insert.move(50, 100)
+        insert.resize(insert.sizeHint())
+        insert.clicked.connect(self.ShowInsert)
+
+
         self.show()
+
 
     def train (self):
         self.setWindowTitle("training")
-        self.move(300,300)
+        self.move(600,300)
         self.setWindowIcon(QIcon('cat.jpg'))
         self.resize(300,600)
-        self.show()
+
+        main = QPushButton('Back to intro', self)
+        main.move(50, 50)
+        main.resize(main.sizeHint())
+        main.clicked.connect(self.ShowMain)
+
+        insert = QPushButton('Insert digit', self)
+        insert.move(50, 100)
+        insert.resize(insert.sizeHint())
+        insert.clicked.connect(self.ShowInsert)
+
     
     def insert (self):
         self.setWindowTitle("inserting digits")
-        self.move(300,300)
+        self.move(900,300)
         self.setWindowIcon(QIcon('cat.jpg'))
         self.resize(300,600)
-        self.show()
+
+        train = QPushButton('train AI', self)
+        train.move(50, 50)
+        train.resize(train.sizeHint())
+        train.clicked.connect(self.ShowTrain)
+
+        main = QPushButton('Back to intro', self)
+        main.move(50, 100)
+        main.resize(main.sizeHint())
+        main.clicked.connect(self.ShowMain)
 
 
 #creates the qapplication
 parent = QApplication(sys.argv)
 
-firstwindow = App(1)
+mainW = App(1)
+trainW = App(2)
+insertW = App(3)
 
 
 #execute the qapplication
