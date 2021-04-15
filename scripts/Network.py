65e40cfc90a0c5489a1b00dc634d9e855a397551
Seamus Stalker <ssta143@aucklanduni.ac.nz>
2021-04-15 21:05:14 +1200

    nn script implemented as per the lab instructions

--- scripts/Network.py
+++ scripts/Network.py
@@ -15,13 +15,14 @@ from torch.jit import script, trace       # hybrid frontend decorator and tracin
 # Training settings
 batch_size = 64
 
-# MNIST Dataset
+# Downloading/locating MNIST Dataset
 
 train_dataset = datasets.MNIST(root='mnist_data_train/', train=True, transform=transforms.ToTensor(), download=False)
 
 test_dataset = datasets.MNIST(root='mnist_data_test/', train=False, transform=transforms.ToTensor(), download=False)
 
 print('*****datasets loaded*****') #DEBUGGING
+
 # Data Loader (Input Pipeline)
 train_loader = data.DataLoader(dataset=train_dataset,
                                            batch_size=batch_size,
@@ -31,6 +32,7 @@ test_loader = data.DataLoader(dataset=test_dataset,
                                           batch_size=batch_size,
                                           shuffle=False)
 
+#Defining the Net class
 class Net(nn.Module):
 
     def __init__(self):
@@ -59,14 +61,57 @@ class Net(nn.Module):
         x = F.relu(self.l9(x))
         return self.l10(x)
 
+# Instantiating a Net object 
 model = Net()
+
+# Setting criterion and optimizer
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.5)
 
+def train(epoch):
+    #Enables updating model weight
+    model.train()
+    # the enumerate function is supported by the DataLoader class, allowing us to traverse train_loader
+    for batch_idx, (data, target) in enumerate(train_loader):
+        optimizer.zero_grad()
+        output = model(data)
+        loss = criterion(output, target)
+        # Updating model weights
+        loss.backward()
+        optimizer.step()
+        #Printing updates as training occurs
+        if batch_idx % 10 == 0:
+            print('Train Epoch: {} | Batch Status: {}/{} ({:.0f}%) | Loss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), loss.item()))
 
 
+def test():
+     #Disables updating model weights
+    model.train(False)
+    test_loss = 0
+    correct = 0
+    for data, target in test_loader:
+        output = model(data)
+        # sum up batch loss
+        test_loss += criterion(output, target).item()
+        # get the index of the max
+        pred = output.data.max(1, keepdim=True)[1]
+        correct += pred.eq(target.data.view_as(pred)).cpu().sum()
 
 
+if __name__ == '__main__':
+    since = time.time()
+    for epoch in range(1, 10):
+        epoch_start = time.time()
+        train(epoch)
+        m, s = divmod(time.time() - epoch_start, 60)
+        print(f'Training time: {m:.0f}m {s:.0f}s')
+        test()
+        m, s = divmod(time.time() - epoch_start, 60)
+        print(f'Testing time: {m:.0f}m {s:.0f}s')
 
+    m, s = divmod(time.time() - since, 60)
+    print(f'Total Time: {m:.0f}m {s:.0f}s')
 
 
