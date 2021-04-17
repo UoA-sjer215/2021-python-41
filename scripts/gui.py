c808a21611f1568248b0ae934768edaaf7eadad1
UoA-sjer215 <68827415+UoA-sjer215@users.noreply.github.com>
2021-04-17 14:36:55 +1200

    update gui interface
    met with teammate and discussed how it should look like

--- scripts/gui.py
+++ scripts/gui.py
@@ -1,94 +1,136 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QToolTip
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QToolTip, QLabel, QVBoxLayout, QGroupBox, QRadioButton, QCheckBox, QPushButton, QMenu, QGridLayout, QVBoxLayout, QProgressBar, QSpinBox
 from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import QCoreApplication
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import Qt
+from PyQt5.QtCore import QBasicTimer
+
+
+epoch = 0
 
 
 class App (QWidget):
-    def __init__ (self,num):
+    def __init__ (self):
         super().__init__()
+        self.main()
 
-        if num==1 :
-            self.main()
-        elif num==2:
-            self.train()
-        elif num==3:
-            self.insert()
-
-
-    def ShowMain (self):
-        self.hide()
-        mainW.show()
-
-    def ShowTrain (self):
-        self.hide()
-        trainW.show()
-
-    def ShowInsert (self):
-        self.hide()
-        insertW.show()
-
-    
+ 
     def main (self):
         self.setWindowTitle("main")
-        self.move(300,300)
+        self.move(500,300)
         self.setWindowIcon(QIcon('cat.jpg'))
-        self.resize(300,600)
+        self.resize(900,600)
 
-        train = QPushButton('train AI', self)
-        train.move(50, 50)
-        train.resize(train.sizeHint())
-        train.clicked.connect(self.ShowTrain)
+        
+        grid = QGridLayout()
+        grid.addWidget(self.preTraining(), 0, 0)
+        grid.addWidget(self.training(), 0, 1)
+        grid.addWidget(self.digitInsert(), 1, 1)
 
-        insert = QPushButton('Insert digit', self)
-        insert.move(50, 100)
-        insert.resize(insert.sizeHint())
-        insert.clicked.connect(self.ShowInsert)
+        self.setLayout(grid)
+
 
 
         self.show()
 
 
-    def train (self):
-        self.setWindowTitle("training")
-        self.move(600,300)
-        self.setWindowIcon(QIcon('cat.jpg'))
-        self.resize(300,600)
+    def preTraining(self):
+        groupbox = QGroupBox('Pre-Training Settings')
 
-        main = QPushButton('Back to intro', self)
-        main.move(50, 50)
-        main.resize(main.sizeHint())
-        main.clicked.connect(self.ShowMain)
 
-        insert = QPushButton('Insert digit', self)
-        insert.move(50, 100)
-        insert.resize(insert.sizeHint())
-        insert.clicked.connect(self.ShowInsert)
+        importTraining = QPushButton('Import')
 
-    
-    def insert (self):
-        self.setWindowTitle("inserting digits")
-        self.move(900,300)
-        self.setWindowIcon(QIcon('cat.jpg'))
-        self.resize(300,600)
+        pixmap = QPixmap('cat.jpg')
+        lbl_img = QLabel()
+        lbl_img.setPixmap(pixmap)
 
-        train = QPushButton('train AI', self)
-        train.move(50, 50)
-        train.resize(train.sizeHint())
-        train.clicked.connect(self.ShowTrain)
+        dispTraining = QPushButton('Display random training image')
+        dispTesting = QPushButton('Display random testing image')
 
-        main = QPushButton('Back to intro', self)
-        main.move(50, 100)
-        main.resize(main.sizeHint())
-        main.clicked.connect(self.ShowMain)
+        vbox = QVBoxLayout()
+        vbox.addWidget(importTraining)
+        vbox.addWidget(lbl_img)
+        vbox.addWidget(dispTraining)
+        vbox.addWidget(dispTesting)
+        
+        vbox.addStretch(1)
+        groupbox.setLayout(vbox)
+
+        return groupbox
+
+    def training(self):
+        groupbox = QGroupBox('Training Settings')
+
+        spinbox = QSpinBox()
+        spinbox.setRange(0, 15)
+        spinbox.setSingleStep(2)        
+        spinbox.valueChanged.connect(self.value_changed)
+
+        lbl1 = QLabel('Epoch Amount')
+        lbl2 = QLabel('(the more you have, the more accurate the model is)')
+
+        train = QPushButton('NO PAIN NO TRAIN')
+
+        pbar = QProgressBar(self)
+        pbar.setGeometry(30, 40, 200, 25)
+
+        btn = QPushButton('Start')
+        btn.move(40, 80)
+
+        
+        
+
+        vbox = QVBoxLayout()
+        vbox.addWidget(lbl1)
+        vbox.addWidget(spinbox)
+        vbox.addWidget(lbl2)
+        vbox.addWidget(train)
+        vbox.addWidget(pbar)
+        vbox.addWidget(btn)
+        groupbox.setLayout(vbox)
+
+        return groupbox
+
+
+    def digitInsert(self):
+        groupbox = QGroupBox('Insert Digits')
+
+        # different push buttons
+        pushbutton = QPushButton('Normal Button')
+        togglebutton = QPushButton('Toggle Button')
+        togglebutton.setCheckable(True)
+        togglebutton.setChecked(True)
+        flatbutton = QPushButton('Flat Button')
+        flatbutton.setFlat(True)
+        popupbutton = QPushButton('Popup Button')
+        menu = QMenu(self)
+        menu.addAction('First Item')
+        menu.addAction('Second Item')
+        menu.addAction('Third Item')
+        menu.addAction('Fourth Item')
+        popupbutton.setMenu(menu)
+
+        vbox = QVBoxLayout()
+        vbox.addWidget(pushbutton)
+        vbox.addWidget(togglebutton)
+        vbox.addWidget(flatbutton)
+        vbox.addWidget(popupbutton)
+        vbox.addStretch(1)
+        groupbox.setLayout(vbox)
+
+        return groupbox
+
+    def value_changed(self):
+        epoch = self.spinbox.value()
+
+
 
 
 #creates the qapplication
 parent = QApplication(sys.argv)
 
-mainW = App(1)
-trainW = App(2)
-insertW = App(3)
+mainW = App()
 
 
 #execute the qapplication
